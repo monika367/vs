9d342c4d3850081902d81c8cd5041053181b9914
monikaku <mo24awka@gmail.com>
2024-03-11 16:09:11 +0530

    first commit

---
+++ my project/coll.py
@@ -0,0 +1,9 @@
+col_name = input('enter college name : ')
+col_id = int(input('enter college id : '))
+col_add = input('enter college address :')
+
+
+print(col_name)
+print(col_id)
+print(col_add)
+
